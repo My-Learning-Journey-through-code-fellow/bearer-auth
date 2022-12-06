7958b3b9a3efcf34feb5c5f007ed97b968b51b9b
Trace Dugar <tracedugar89@gmail.com>
2022-12-06 15:34:20 -0800

    Starter code import

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const PORT = process.env.PORT || 3001;
+
+// Start up DB Server
+const { db } = require('./src/auth/models/index.js');
+db.sync()
+  .then(() => {
+
+    // Start the web server
+    require('./src/server.js').start(process.env.PORT);
+  });
